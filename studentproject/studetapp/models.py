32dcc930c0b90e7a0ce5f73b839e2c50ecb71a4c
devBibek <Syandenbibek@gmail.com>
2026-03-29 18:00:55 +0545

    Django student project initial commit

--- studentproject/studetapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Student(models.Model):
-    student_name = models.CharField(max_length=50)
-    student_age = models.IntegerField()
-    student_roll = models.IntegerField()
-    student_addre = models.CharField(max_length=100)
